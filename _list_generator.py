a8acc7be80c863d151f7f7ac61f00542c34750c2
LucasPeixotg <Lucaspeixotopro@gmail.com>
2024-03-09 15:07:06 -0300

    images fixed and lighter theme added

--- _list_generator.py
+++ _list_generator.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     items = listdir("./image/gifts")
 
     for item in items:
-        print("<li class=\"card bg-dark col-10 col-md-4 mb-4 col-lg-3 border-none\">")
+        print("<li class=\"card bg-none col-10 col-md-4 mb-4 col-lg-3 border-none\">")
         print(f"\t<img src=\"image/gifts/{item}\" class=\"card-img-top border-inverse-cup\" alt=\"Foto de {item.split('.')[0]}\">")
-        print("\t<div class=\"card-body border-cup rounded-bottom\">")
-        print(f"\t\t<p class=\"card-text text-center fs-3 montserrat\">{item.split('.')[0]}</p>")
+        print("\t<div class=\"card-body bg-accent border-cup rounded-bottom\">")
+        print(f"\t\t<p class=\"card-text text-center white fs-3 montserrat\">{item.split('.')[0]}</p>")
         print("\t</div>")
         print("</li>")
