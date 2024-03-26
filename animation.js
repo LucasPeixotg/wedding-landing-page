c0c144461f8f49c11c9cac1fbc44cf5a8e911bf4
LucasPeixotg <Lucaspeixotopro@gmail.com>
2024-03-26 11:04:31 -0300

    song permission added

--- animation.js
+++ animation.js
@@ -1,15 +1,8 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if(entry.isIntersecting && entry.target.classList.contains("observer-left")) entry.target.classList.add("show");
-        else if(entry.isIntersecting && entry.target.classList.contains("audio-play")) {
-            entry.target.classList.remove("audio-play")
-            const audio = document.querySelector("audio")
-            audio.volume = 0.02
-            audio.play()
-        }
     })
 })
 
 const hiddenElements = document.querySelectorAll('.observer-left')
 hiddenElements.forEach(el => observer.observe(el))
-observer.observe(document.querySelector(".audio-play"))
